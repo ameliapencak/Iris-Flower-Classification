3665b71beb349201026e8c6b6b25467d152d8f61
Amelia <amelia.pencak@gmail.com>
2024-10-11 23:16:57 +0200

    odczyt danych z pliku

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
+# DataFlair Iris Flower Classification
+# Import Packages
+import sqlite3
+#import numpy as np
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+iris = pd.read_csv("data/Iris.csv",sep=',')
+print(iris.head())
+# Ścieżka do pliku SQLite w folderze projektu
+sqlite_path = 'data/database.sqlite'
+
+# Połączenie z bazą danych SQLite
+conn = sqlite3.connect(sqlite_path)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+conn.close()
